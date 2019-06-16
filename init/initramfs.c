b8f0ed95a96e7be57ab49938fa95ad7ba82feb28
Nanda Okitavera <codeharuka.yusa@gmail.com>
2019-06-16 21:03:03 +0700

    initramfs: load ramdisk dynamically
    This patch will ignores the skip_initramfs initdata and letting the kernel itself
    to load the ramdisk whether it present or not, so any applications on the ramdisk
    department are still working without any anti-pattern and proprietary-ways
    hacks like hexpatching the kernel binary.
    
    Signed-off-by: Nanda Okitavera <codeharuka.yusa@gmail.com>
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- init/initramfs.c
+++ init/initramfs.c
@@ -603,6 +603,9 @@ static int __init populate_rootfs(void)
 {
 	/* Load the built in initramfs */
 	char *err = unpack_to_rootfs(__initramfs_start, __initramfs_size);
+
+	bool skip_ramdisk;
+
 	if (err)
 		panic("%s", err); /* Failed to decompress INTERNAL initramfs */
 	/* If available load the bootloader supplied initrd */
@@ -612,8 +615,11 @@ static int __init populate_rootfs(void)
 		printk(KERN_INFO "Trying to unpack rootfs image as initramfs...\n");
 		err = unpack_to_rootfs((char *)initrd_start,
 			initrd_end - initrd_start);
-		if (!err)
+
+		if (!err) {
+			skip_ramdisk = true;
 			goto done;
+		}
 
 		clean_rootfs();
 		unpack_to_rootfs(__initramfs_start, __initramfs_size);
@@ -638,8 +644,10 @@ static int __init populate_rootfs(void)
 		printk(KERN_INFO "Unpacking initramfs...\n");
 		err = unpack_to_rootfs((char *)initrd_start,
 			initrd_end - initrd_start);
-		if (err)
+		if (err) {
 			printk(KERN_EMERG "Initramfs unpacking failed: %s\n", err);
+			skip_ramdisk = true;
+		}
 #endif
 	}
 	free_initrd();
@@ -650,6 +658,6 @@ static int __init populate_rootfs(void)
 	 */
 	load_default_modules();
 
-	return 0;
+	return skip_ramdisk ? default_rootfs() : 0;
 }
 rootfs_initcall(populate_rootfs);
