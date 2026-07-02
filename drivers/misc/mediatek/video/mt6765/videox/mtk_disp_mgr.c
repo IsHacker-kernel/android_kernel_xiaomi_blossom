75c2268e1e0ffdde6f08ef1c879c56204a2b2b80
danya2271 <dany2271@yandex.ru>
2026-07-02 13:08:30 +0500

    video: skip redundant caps ioctl copy

--- drivers/misc/mediatek/video/mt6765/videox/mtk_disp_mgr.c
+++ drivers/misc/mediatek/video/mt6765/videox/mtk_disp_mgr.c
@@ -1155,14 +1155,9 @@ int _ioctl_get_is_driver_suspend(unsigned long arg)
 int _ioctl_get_display_caps(unsigned long arg)
 {
 	int ret = 0;
-	struct disp_caps_info caps_info;
+	struct disp_caps_info caps_info = {0};
 	void __user *argp = (void __user *)arg;
 
-	if (copy_from_user(&caps_info, argp, sizeof(caps_info))) {
-		DISPERR("[FB]: copy_from_user failed! line:%d\n", __LINE__);
-		ret = -EFAULT;
-	}
-	memset(&caps_info, 0, sizeof(caps_info));
 #ifdef DISP_HW_MODE_CAP
 	caps_info.output_mode = DISP_HW_MODE_CAP;
 #else
