386c91214b967a4e89657a7bd0f9e8f84100e24d
IsHacker <83446126+IsHacker003@users.noreply.github.com>
2026-08-04 08:40:23 -0300

    Update compile_dandelion_ksu.sh

--- .IsHacker-kernel/compile_dandelion_ksu.sh
+++ .IsHacker-kernel/compile_dandelion_ksu.sh
@@ -16,4 +16,4 @@ export CROSS_COMPILE="aarch64-linux-gnu-"
 export CC="clang"
 export CLANG_TRIPLE="aarch64-linux-gnu-"
 make O=out CC=clang ARCH=${ARCH} ${KERNEL_DEFCONFIG}
-make -j5 KCFLAGS="-O3 -ffast-math -mcpu=cortex-a53 -mtune=cortex-a53 -mfpu=asimd-vfpv4 -march=armv8-a" O=out ARCH=${ARCH} CC=clang CLANG_TRIPLE=${CLANG_TRIPLE} CROSS_COMPILE=${CROSS_COMPILE} LD=ld.lld NM=llvm-nm AR=llvm-ar
+make -j$(nproc) KCFLAGS="-O3 -ffast-math -mcpu=cortex-a53 -mtune=cortex-a53 -mfpu=asimd-vfpv4 -march=armv8-a" O=out ARCH=${ARCH} CC=clang CLANG_TRIPLE=${CLANG_TRIPLE} CROSS_COMPILE=${CROSS_COMPILE} LD=ld.lld NM=llvm-nm AR=llvm-ar
