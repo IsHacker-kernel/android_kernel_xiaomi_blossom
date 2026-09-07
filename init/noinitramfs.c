6da97bf7914c28f3332e22c9619717c3ae996a40
IsHacker <83446126+IsHacker003@users.noreply.github.com>
2026-09-07 17:38:38 +0200

    init: initramfs: export default_rootfs()

--- init/noinitramfs.c
+++ init/noinitramfs.c
@@ -21,11 +21,16 @@
 #include <linux/stat.h>
 #include <linux/kdev_t.h>
 #include <linux/syscalls.h>
+#include <linux/initramfs.h>
+#include <linux/kconfig.h>
 
 /*
  * Create a simple rootfs that is similar to the default initramfs
  */
-static int __init default_rootfs(void)
+#if !IS_BUILTIN(CONFIG_BLK_DEV_INITRD)
+static
+#endif
+int __init default_rootfs(void)
 {
 	int err;
 
