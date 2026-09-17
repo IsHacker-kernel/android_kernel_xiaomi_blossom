07b0c6161dfc1c8c7324e8ec9eccc49fcf5d9f82
IsHacker003 <83446126+IsHacker003@users.noreply.github.com>
2026-09-18 00:50:04 +0200

    IsHacker-kernel: Use AOSP clang as CC and switch to rsuntk's KSU

--- .IsHacker-kernel/compile_dandelion_ksu.sh
+++ .IsHacker-kernel/compile_dandelion_ksu.sh
@@ -5,23 +5,19 @@
 
 export KERNEL_DEFCONFIG="blossom_defconfig"
 git clone https://gitlab.com/clangsantoni/zyc_clang clang --depth=1 --branch 21 /tmp/clang
-export PATH="/tmp/clang/bin:${PATH}"
+git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 --depth=1 /tmp/aosp-clang
+export PATH="/tmp/aosp-clang/clang-r614150/bin:${PATH}"
 clang --version
 
-# Stalled
-# chmod u+x clean.sh
-# bash clean.sh
+chmod u+x clean.sh
+bash clean.sh
 
-curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
-
-# Stalled
-# Fix CONFIG_KPROBES dependency and several other problems with KSU
-# patch -p1 -i ksu_blossom.patch
+curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -
 
 export KERNEL_ARCH="arm64"
 export ARCH=${KERNEL_ARCH}
-export CROSS_COMPILE="aarch64-linux-gnu-"
+export CROSS_COMPILE="/tmp/clang/bin/aarch64-linux-gnu-"
 export CC="clang"
-export CLANG_TRIPLE="aarch64-linux-gnu-"
+export CLANG_TRIPLE="/tmp/clang/bin/aarch64-linux-gnu-"
 make O=out CC=clang ARCH=${ARCH} ${KERNEL_DEFCONFIG}
 make -j$(nproc) KCFLAGS="-Wno-error=implicit-function-declaration" O=out ARCH=${ARCH} CC=clang CLANG_TRIPLE=${CLANG_TRIPLE} CROSS_COMPILE=${CROSS_COMPILE} LD=ld.lld NM=llvm-nm AR=llvm-ar
--- .IsHacker-kernel/compile_dandelion_noksu.sh
+++ .IsHacker-kernel/compile_dandelion_noksu.sh
@@ -6,12 +6,14 @@
 export KERNEL_DEFCONFIG="blossom_defconfig"
 export KERNEL_ARCH="arm64"
 sed -i -e 's/CONFIG_KSU=y/# CONFIG_KSU is not set/g' arch/${KERNEL_ARCH}/configs/${KERNEL_DEFCONFIG}
+rm -rf KernelSU
 git clone https://gitlab.com/clangsantoni/zyc_clang clang --depth=1 --branch 21 /tmp/clang
-export PATH="/tmp/clang/bin:${PATH}"
+git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 --depth=1 /tmp/aosp-clang
+export PATH="/tmp/aosp-clang/clang-r614150/bin:${PATH}"
 clang --version
 export ARCH=${KERNEL_ARCH}
-export CROSS_COMPILE="aarch64-linux-gnu-"
+export CROSS_COMPILE="/tmp/clang/bin/aarch64-linux-gnu-"
 export CC="clang"
-export CLANG_TRIPLE="aarch64-linux-gnu-"
+export CLANG_TRIPLE="/tmp/clang/bin/aarch64-linux-gnu-"
 make O=out CC=clang ARCH=${ARCH} ${KERNEL_DEFCONFIG}
 make -j$(nproc) KCFLAGS="-Wno-error=implicit-function-declaration" O=out ARCH=${ARCH} CC=clang CLANG_TRIPLE=${CLANG_TRIPLE} CROSS_COMPILE=${CROSS_COMPILE} LD=ld.lld NM=llvm-nm AR=llvm-ar
