7dfad4a736f9c8b52528bb81fcfbcfd796d4508a
IsHacker003 <83446126+IsHacker003@users.noreply.github.com>
2026-09-17 18:04:14 +0200

    IsHacker-kernel: Fix implicit function declarations

--- .IsHacker-kernel/compile_dandelion_noksu.sh
+++ .IsHacker-kernel/compile_dandelion_noksu.sh
@@ -14,4 +14,4 @@ export CROSS_COMPILE="aarch64-linux-gnu-"
 export CC="clang"
 export CLANG_TRIPLE="aarch64-linux-gnu-"
 make O=out CC=clang ARCH=${ARCH} ${KERNEL_DEFCONFIG}
-make -j$(nproc) KCFLAGS="-O3 -ffast-math -mcpu=cortex-a53 -mtune=cortex-a53 -march=armv8-a" O=out ARCH=${ARCH} CC=clang CLANG_TRIPLE=${CLANG_TRIPLE} CROSS_COMPILE=${CROSS_COMPILE} LD=ld.lld NM=llvm-nm AR=llvm-ar
+make -j$(nproc) KCFLAGS="-Wno-error=implicit-function-declaration" O=out ARCH=${ARCH} CC=clang CLANG_TRIPLE=${CLANG_TRIPLE} CROSS_COMPILE=${CROSS_COMPILE} LD=ld.lld NM=llvm-nm AR=llvm-ar
