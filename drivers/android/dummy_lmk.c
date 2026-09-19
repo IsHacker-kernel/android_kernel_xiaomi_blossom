40e74b856acb1a5a4e2f4061d19d5fbc74ffa5e3
IsHacker003 <83446126+IsHacker003@users.noreply.github.com>
2026-09-19 16:39:13 +0200

    drivers/android: Add Dummy LMK driver

---
+++ drivers/android/dummy_lmk.c
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2025 IsHacker
+ */
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+
+static const char *minfree = "0,0,0,0,0,0";
+static const int adj = 1001;
+
+module_param(minfree, charp, 0444);
+module_param(adj, int, 0444);
+
+
+static int __init dummy_lmk_init(void)
+{
+	return 0;
+}
+
+static void __exit dummy_lmk_exit(void)
+{
+	return;
+}
+
+module_init(dummy_lmk_init);
+module_exit(dummy_lmk_exit);
+
+MODULE_AUTHOR("IsHacker");
+MODULE_LICENSE("GPL");
