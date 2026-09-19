d79d6c2b15ce0af89049ffc1c541c1d9f0948529
IsHacker <83446126+IsHacker003@users.noreply.github.com>
2026-09-19 15:14:15 -0600

    fs: exec: fix ksu hook

--- fs/exec.c
+++ fs/exec.c
@@ -1734,6 +1734,11 @@ static int __do_execve_file(int fd, struct filename *filename,
 	struct files_struct *displaced;
 	int retval;
 
+#ifdef CONFIG_KSU
+	extern int ksu_handle_execveat(int *, struct filename **, void *, void *, int *);
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
++#endif
+
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
 
@@ -1907,11 +1912,6 @@ int do_execve_file(struct file *file, void *__argv, void *__envp)
 	return __do_execve_file(AT_FDCWD, NULL, argv, envp, 0, file);
 }
 
-#ifdef CONFIG_KSU
-__attribute__((hot))
-extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr,
-			void *argv, void *envp, int *flags);
-#endif
 
 int do_execve(struct filename *filename,
 	const char __user *const __user *__argv,
@@ -1919,9 +1919,7 @@ int do_execve(struct filename *filename,
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
-#ifdef CONFIG_KSU
-	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
-#endif
+
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
@@ -1949,9 +1947,7 @@ static int compat_do_execve(struct filename *filename,
 		.is_compat = true,
 		.ptr.compat = __envp,
 	};
-#ifdef CONFIG_KSU
-	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
-#endif
+
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
